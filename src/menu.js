4624c2cbb010cf4482e3b86f9f9bde8c83a6930b
AnastasiiaNotych <notychwork@gmail.com>
2025-10-14 02:12:49 +0200

    виправлення помилок

--- src/menu.js
+++ src/menu.js
@@ -1,22 +1,3 @@
-// (() => {
-//     const refs = {
-//         // Додати атрибут data-modal-open на кнопку відкриття
-//         openModalBtn: document.querySelector('[data-menu-open]'),
-//         // Додати атрибут data-modal-close на кнопку закриття
-//         closeModalBtn: document.querySelector('[data-menu-close]'),
-//         // додати атрибут data-modal на бекдроп модалки
-//         modal: document.querySelector('[data-menu]'),
-//     };
-//     refs.openModalBtn.addEventListener('click', toggleModal);
-//     refs.closeModalBtn.addEventListener('click', toggleModal);
-
-//     function toggleModal() {
-//         /is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-//         refs.modal.classList.toggle('is-open');
-//         //  document.body.classList.toggle('no-scroll', refs.modal.classList.contains('is-open'));
-//     }
-// })();
-
 (() => {
   const refs = {
     openBtn: document.querySelector('[data-menu-open]'),
@@ -35,4 +16,10 @@
     document.body.classList.toggle('no-scroll', open);
     // refs.btn?.classList.toggle('is-open', open);
   }
+  refs.menu.addEventListener('click', e => {
+    if (e.target.closest('a')) {
+      refs.menu.classList.remove('is-open');
+      document.body.classList.remove('no-scroll');
+    }
+  });
 })();
