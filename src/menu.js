b4afd8152464da942b0e935be9cf3f6715db8f82
Anastasiia Notych <notychwork@gmail.com>
2025-10-14 11:41:25 +0200

    upgrade styles
    поркащення запису підключення стилів, додавання аріа лейбл для кнопок і посилань

--- src/menu.js
+++ src/menu.js
@@ -3,7 +3,6 @@
     openBtn: document.querySelector('[data-menu-open]'),
     closeBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
-    btn: document.querySelector('.header-button'),
   };
 
   if (!refs.openBtn || !refs.closeBtn || !refs.menu) return;
@@ -14,12 +13,20 @@
   function toggle() {
     const open = refs.menu.classList.toggle('is-open');
     document.body.classList.toggle('no-scroll', open);
-    // refs.btn?.classList.toggle('is-open', open);
+
+    refs.openBtn.setAttribute('aria-expanded', open);
+    refs.openBtn.setAttribute(
+      'aria-label',
+      open ? 'Закрити меню' : 'Відкрити меню'
+    );
   }
+
   refs.menu.addEventListener('click', e => {
     if (e.target.closest('a')) {
       refs.menu.classList.remove('is-open');
       document.body.classList.remove('no-scroll');
+      refs.openBtn.setAttribute('aria-expanded', false);
+      refs.openBtn.setAttribute('aria-label', 'Відкрити меню');
     }
   });
 })();
